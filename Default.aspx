01c892cb18dd8cc736e63cc75ce10537ed3c91fa
masoud.sojodi <masoud.sojodi@gmail.com>
2016-09-14 10:20:30 +0430

    q

---
+++ Default.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView runat="server" ID="GridView11"></asp:GridView>
+
+        </div>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" DataKeyNames="Id"
+OnRowDataBound="OnRowDataBound" OnRowEditing="OnRowEditing" OnRowCancelingEdit="OnRowCancelingEdit"
+OnRowUpdating="OnRowUpdating" OnRowDeleting="OnRowDeleting" EmptyDataText="No records has been added.">
+<Columns>
+    <asp:TemplateField HeaderText="Name" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lblName" runat="server" Text='<%# Eval("Name") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtName" runat="server" Text='<%# Eval("Name") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+    <asp:TemplateField HeaderText="Country" ItemStyle-Width="150">
+        <ItemTemplate>
+            <asp:Label ID="lblCountry" runat="server" Text='<%# Eval("email") %>'></asp:Label>
+        </ItemTemplate>
+        <EditItemTemplate>
+            <asp:TextBox ID="txtCountry" runat="server" Text='<%# Eval("email") %>'></asp:TextBox>
+        </EditItemTemplate>
+    </asp:TemplateField>
+    <asp:CommandField ButtonType="Link" ShowEditButton="true" ShowDeleteButton="true" ItemStyle-Width="150"/>
+</Columns>
+</asp:GridView>
+<table border="1" cellpadding="0" cellspacing="0" style="border-collapse: collapse">
+<tr>
+    <td style="width: 150px">
+        Name:<br />
+        <asp:TextBox ID="txtName" runat="server" Width="140" />
+    </td>
+    <td style="width: 150px">
+        Country:<br />
+        <asp:TextBox ID="txtCountry" runat="server" Width="140" />
+    </td>
+    <td style="width: 100px">
+        <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="Insert" />
+    </td>
+</tr>
+</table>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            this.BindGrid();
+        }
+
+        if (!this.IsPostBack)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (MySqlConnection con = new MySqlConnection(constr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM users"))
+                {
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                    {
+                        
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            GridView11.DataSource = dt;
+                            GridView11.DataBind();
+                        }
+                    }
+                }
+            }
+        }
+    }
+    protected void Insert(object sender, EventArgs e)
+    {
+        string name = txtName.Text;
+        string country = txtCountry.Text;
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            using (MySqlCommand cmd = new MySqlCommand("INSERT INTO users (name, email) VALUES (@email, @email)"))
+            {
+                using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                {
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@email", country);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+        this.BindGrid();
+    }
+    protected void OnRowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        GridViewRow row = GridView1.Rows[e.RowIndex];
+        int Id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+        string name = (row.FindControl("txtName") as TextBox).Text;
+        string email = (row.FindControl("txtCountry") as TextBox).Text;
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            using (MySqlCommand cmd = new MySqlCommand("UPDATE users SET name = @name, email = @email WHERE Id = @Id"))
+            {
+                using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                {
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+        GridView1.EditIndex = -1;
+        this.BindGrid();
+    }
+    protected void OnRowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        this.BindGrid();
+    }
+    protected void OnRowCancelingEdit(object sender, EventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        this.BindGrid();
+    }
+    protected void OnRowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow && e.Row.RowIndex != GridView1.EditIndex)
+        {
+            (e.Row.Cells[2].Controls[2] as LinkButton).Attributes["onclick"] = "return confirm('Do you want to delete this row?');";
+        }
+    }
+    protected void OnRowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        int Id = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Values[0]);
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            using (MySqlCommand cmd = new MySqlCommand("DELETE FROM users WHERE Id = @Id"))
+            {
+                using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                {
+                    cmd.Parameters.AddWithValue("@Id", Id);
+                    cmd.Connection = con;
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+        }
+        this.BindGrid();
+    }
+    private void BindGrid()
+    {
+        string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+        using (MySqlConnection con = new MySqlConnection(constr))
+        {
+            using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM users"))
+            {
+                using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                {
+                    cmd.Connection = con;
+                    sda.SelectCommand = cmd;
+                    using (DataTable dt = new DataTable())
+                    {
+                        sda.Fill(dt);
+                        GridView1.DataSource = dt;
+                        GridView1.DataBind();
+                    }
+                }
+            }
+        }
+    }
+}
